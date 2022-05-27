09d9f152d45868f49acbc9eb8608a0138bacb68b
hfenelsoftllc <hfenelsoftllc@outlook.com>
2022-05-27 03:54:56 -0400

    Create route table and assoc

--- main.tf
+++ main.tf
@@ -27,3 +27,21 @@ resource "aws_internet_gateway" "lakay_igw" {
   }
 }
 
+resource "aws_route_table" "lakay_rt_public" {
+  vpc_id = aws_vpc.lakay_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.lakay_rt_public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.lakay_igw.id
+}
+
+resource "aws_route_table_association" "lakay_pub_assoc" {
+  subnet_id      = aws_subnet.lakay_public_subnet.id
+  route_table_id = aws_route_table.lakay_rt_public.id
+}
