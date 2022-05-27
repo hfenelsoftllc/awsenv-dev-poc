cb42040f3298f8f18d6a27f5d9a95d490cfdb340
hfenelsoftllc <hfenelsoftllc@outlook.com>
2022-05-27 03:22:22 -0400

    Create subnet to host the ec2 instance

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
 resource "aws_vpc" "lakay_vpc" {
-  cidr_block          = "10.123.0.0/16"
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support  = true
+  enable_dns_support   = true
 
   tags = {
     Name = "dev-env"
   }
 }
+
+resource "aws_subnet" "lakay_public_subnet" {
+  vpc_id                  = aws_vpc.lakay_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "lakay-dev-pubsub"
+  }
+}
+
